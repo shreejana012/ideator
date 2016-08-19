ce88cd2a11534c43c4723cc408aa285aba73e3ab
shreejana012 <shrijana460@gmail.com>
2016-08-19 15:47:11 +0545

    adding ideas

---
+++ db/migrate/20160818134828_create_ideas.rb
@@ -0,0 +1,9 @@
+class CreateIdeas < ActiveRecord::Migration[5.0]
+  def change
+    create_table :ideas do |t|
+        t.text :description
+        t.string :author
+        t.timestamps null: false
+    end
+  end
+end
